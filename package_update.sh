aa7fdc01f9a47d867913583b87870d0eba2560d9
sunabove <sunabove@nate.com>
2024-03-16 12:53:33 +0900

    Create package_update.sh

---
+++ package_update.sh
@@ -0,0 +1,23 @@
+# package update #
+
+sudo apt install python3-pip -y
+
+sudo apt install python-is-python3  -y
+
+sudo apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev  -y
+
+sudo apt install gettext -y
+
+sudo apt install librsync-dev -y
+
+sudo apt install libcairo2-dev -y
+
+sudo apt install libsystemd-dev -y
+
+sudo pip3 install dbus-python
+
+sudo pip3 install pipdate
+
+sudo python3 -m pip install pip --upgrade
+
+sudo -H pipdate
