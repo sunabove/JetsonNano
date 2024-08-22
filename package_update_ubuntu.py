dd4dcfb10d2ce4fa139fb58395b2b1256eefb98c
sunabove <sunabove@nate.com>
2024-08-22 19:34:47 +0900

    Update package_update_ubuntu.py

--- package_update_ubuntu.py
+++ package_update_ubuntu.py
@@ -5,77 +5,99 @@ cmds = """
 # sudo vi /var/lib/dpkg/status
 # sudo vi /var/lib/dpkg/available
 
-sudo apt purge libreoffice* -y
+#sudo apt purge libreoffice* -y
 
-sudo apt purge firefox* -y
+#sudo apt purge firefox* -y
 
+# vmware 데스크톱 툴 설치
+sudo apt install open-vm-tools-desktop
+
+# 프로그램 목록 업데이트
 sudo apt update
 
+# 프로그램 업그레이드
 sudo apt upgrade -y
 
-sudo apt install --only-upgrade tensorrt
-
-sudo apt install python3-setuptools -y
-
+# 불필요 프로그램 삭제
 sudo apt autoremove -y
 
+# 필요시 재부팅
+# sudo reboot
+
+# python으로 python3 명령어 실행하는 프로그램
+sudo apt install python-is-python3
+
+# python pip 패키지 관리자 설치
 sudo apt install python3-pip -y
 
+# 파이썬 설정툴 설치
+sudo apt install python3-setuptools -y
+
+# 소프트웨어 소스 관리 도구 설치
 sudo apt install software-properties-common -y
 
-sudo apt install python-is-python3 -y
+# 빌드 도구 설치
+sudo apt install build-essential -y
 
-sudo apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev  -y
+# D-Bus API 설치
+sudo apt install libdbus-glib-1-dev -y
 
+# GObject Introspection을 지원하는 개발 패키지
+sudo apt install libgirepository1.0-dev -y
+
+# 파이썬 국제화(I18N)와 현지화(L10N) 서비스를 제공
 sudo apt install gettext -y
 
+# rsync 패키지 설치
 sudo apt install librsync-dev -y
 
+# cairo2 패키지 설치
 sudo apt install libcairo2-dev -y
 
+# systemd 패키지 설치
 sudo apt install libsystemd-dev -y
 
+# cups2 패키지 설치
 sudo apt install libcups2-dev -y
 
 # sudo apt install python3-pyqt5 -y
 # sudo apt purge python3-pyqt5
 
+# openblas 개발 패키지 설치
 sudo apt install libopenblas-dev -y
 
-sudo python3 -m pip install pip --upgrade
+# 파이썬 EXTERNALLY 제거
+sudo rm /usr/lib/python3*/EXTERN*
 
+# pip 패키지 관리자 업그레이드
+sudo pip3 install pip --upgrade 
+
+# dbus 패키지 설치
 sudo pip3 install dbus-python
 
+# packaging 패키지 설치
 sudo pip3 install packaging
 
+# pipdate 패키지 업데이트 관리자 설치
 sudo pip3 install pipdate
 
+# json-stats 패키지 설치
 sudo pip3 install jetson-stats
 
+# qtbase5 패키지 설치
 sudo apt install qtbase5-dev -y
 
+# samba 관련 프로그램 설치
 sudo apt install smbclient libmaus2-2 libmaus2-dev -y
 
+# glib2 관련 라이브러리 설치
 sudo apt install libglib2.0-dev libsmbclient-dev libcups2-dev -y
 
-sudo -H pipdate
-
-# pytorch
-
-wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -O torch-1.8.0-cp36-cp36m-linux_aarch64.whl -O torch-1.9.0-cp36-cp36m-linux_aarch64.whl
-sudo apt install libopenblas-base libopenmpi-dev -y
-sudo -H pip3 install Cython
-sudo -H pip3 install torch-1.9.0-cp36-cp36m-linux_aarch64.whl
-
-# sudo ubuntu-drivers autoinstall
-
-# sudo apt install nvidia-cuda-toolkit -y
-
-# sudo apt install cuda-toolkit -y
-
-# sudo pip3 install torch torchvision torchaudio
-
+# 불필요 프로그램 삭제
 sudo apt autoremove -y
+
+# 모든 파이썬 패키지 업데이트
+sudo -H pipdate
 
 """
 
@@ -89,7 +111,11 @@ for idx, cmd in enumerate( cmds ) :
     if len( cmd ) == 0 :
         continue
     elif cmd.startswith( "#" ) :
-        continue
+        if "sudo" in cmd :
+            pass
+        else :
+            print( cmd, flush=1 )
+        pass
     pass
 
     if idx : print()
